027c47ad957c6db0fe4e871df5fddb9f0cba8590
delaneyb22 <delaneybaker8@gmail.com>
2023-02-20 18:29:48 -0600

    move files

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+       //scriptureManager s1=new scriptureManager();
+       //word w=new word();
+        Console.Write("please enter scripture reference: ");
+        string reference = Console.ReadLine();
+        Console.WriteLine("please enter scripture text ");
+        string scripture=Console.ReadLine();
+        scriptureManager brkScrpt = new scriptureManager();
+        List<string> brokenScripture = brkScrpt.ScriptureBreaker(scripture);
+       // Console.Write("example of scritpure");
+       // Console.ReadLine();
+        Console.Clear();
+        Console.WriteLine(scripture);
+        Console.WriteLine(reference);
+        Console.WriteLine("press enter to hide random words until you have the scripture memorized. Or type quit to end program.");
+        
+        string userValue;
+        do
+        {
+            Console.WriteLine(scripture+ reference);
+            Console.WriteLine("Whatever you type(needs to change to ), You will get same Output");
+            userValue = Console.ReadLine();
+            Console.WriteLine("Your written Words Are: " + userValue);
+        }
+        while(userValue != "quit");
+        
+
+
+
+    
+
+       
     }
+
 }
